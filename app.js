2c4ed373659afc5453d2ff2a277f3ffad8f2c502
Donenaziba Josiah <jdonenaziba@gmail.com>
2024-07-16 23:49:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require("express");
+const db = require("./config/db");
+const dotenv = require("dotenv");
+const payment = require("./routes/payment.routes");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//dotenv.config({ path: "./config/config.env" });
+
+db();
+
+// routes
+app.use("/api/PayStackDemo/pay", payment);
+
+// Body parser
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server starting on port: ${port}`);
+});
---
+++ payment.routes.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const { startPayment, createPayment, getPayment } = require('./controllers/payment');
+
+const router = express.Router()
+
+router.post('/', startPayment );
+router.get('/createPayment', createPayment);
+router.get('/paymentDetails', getPayment);
+
+module.exports = router;
